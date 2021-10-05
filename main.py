370eea529466be9bcb95bc1fe9cb3844202b1d97
minsky <mingorun@163.com>
2021-10-05 22:46:24 +0800

    测试模块使用

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from module_A.fibo import *
+import module_A.fibo
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +15,26 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
+    fib(500)
+    print(dir(module_A.fibo))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# __all__ :
+
+# __builtins__ :
+
+# __cached__ :
+
+# __doc__ :
+
+# __file__ :
+
+# __loader__ :
+
+# __name__ :
+
+# __package__ :
+
+# __spec__ :
---
+++ module_A/fibo.py
@@ -0,0 +1,21 @@
+# 斐波那契(fibonacci)数列模块
+
+# 通过__all__ 指定当期模块(fibo.py)下能被导入的模块
+__all__ = ["fib"]
+
+
+def fib(n):  # 定义到 n 的斐波那契数列
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a + b
+    print()
+
+
+def fib2(n):  # 返回到 n 的斐波那契数列
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+    return result
